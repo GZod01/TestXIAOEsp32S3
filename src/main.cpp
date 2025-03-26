d46fd2a857bd5eaa8bc5d1b686710a9e0d4fbdc2
gzod01 <gzod01@gzod01.fr>
2025-03-26 19:39:13 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include <RPLidar.h>
+#include <string>
+RPLidar lidar;
+HardwareSerial lidarSerial(1);
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lidar.begin(lidarSerial);
+  Serial.begin(115200);
+  Serial.println("Hello WOrld");
+  analogWrite(0,125);
 }
 
+typedef struct RPLidarMeasurement RPLIDARRES;
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(IS_OK(lidar.waitPoint())){
+    RPLIDARRES mesureRes = lidar.getCurrentPoint();
+    if(mesureRes.quality>14){
+      Serial.print("Distance: ");Serial.print(mesureRes.distance);Serial.print("Angle: ");Serial.print(mesureRes.angle);Serial.print("Quality: ");Serial.print(mesureRes.quality);Serial.print("\n");
+    }
+  }
+  Serial.println("Hello");
+  delay(500);
 }
